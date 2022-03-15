1758e20146dae8ad75f41f0e511aef9d6d3cf499
Ephraim19 <machayoephraim@gmail.com>
2022-03-15 08:48:19 +0300

    Updated deployment data

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,15 +8,27 @@ import java.util.List;
 import java.util.Map;
 
 public class App {
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
     public static void main(String[] args) {
+        port(getHerokuAssignedPort());
+
         Sql2oUserDao userDao;
         Sql2oNewsDao newsDao;
         Sql2oDepartmentDao departmentDao;
         Connection conn;
         Gson gson = new Gson();
 
-        String connectionString = "jdbc:postgresql://localhost:5432/news_api";
-        Sql2o sql2o = new Sql2o(connectionString, "ephu17", "ephu17");
+//        String connectionString = "jdbc:postgresql://localhost:5432/news_api";
+//        Sql2o sql2o = new Sql2o(connectionString, "ephu17", "ephu17");
+
+        String connectionString = "jdbc:postgresql://ec2-44-194-167-63.compute-1.amazonaws.com;5432/da0v6pcmgs8d87";
+        Sql2o sql2o = new Sql2o(connectionString, "oqwkhjkocsrmvg", "2f8eacbc3b57758a99ccfb6b68f953bc32c170ad0d00aed5b5720fb1144fbcaa");
 
         userDao = new Sql2oUserDao(sql2o);
         newsDao = new Sql2oNewsDao(sql2o);
